8d671bf03a5ad4cc01390922f5f03406f591eeeb
Alex Wilson <fonzmp@gmail.com>
2018-06-19 21:39:02 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,6 +7,9 @@ class SongsController < Sinatra::Base
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
+  enable :sessions
+  use Rack::Flash
+
   get '/songs/new' do
     erb :"/songs/new"
   end
