906da2d325c3ea3a38470aeff115baf651d9e78f
Alex Wilson <fonzmp@gmail.com>
2018-06-19 22:13:22 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -52,7 +52,6 @@ class SongsController < Sinatra::Base
   post '/songs/:slug' do
     @artist = Artist.find(params[:song][:artist_id])
     @song = Song.find_by(name: params[:song][:name])
-    puts @artist
-    puts @song
+    @artist.songs << @song
   end
 end
