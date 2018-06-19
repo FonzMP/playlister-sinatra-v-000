9c10b77cf02352298af456bd3e757cabad070cc8
Alex Wilson <fonzmp@gmail.com>
2018-06-19 22:14:04 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -53,5 +53,7 @@ class SongsController < Sinatra::Base
     @artist = Artist.find(params[:song][:artist_id])
     @song = Song.find_by(name: params[:song][:name])
     @artist.songs << @song
+
+    redirect "/songs/#{@song.slug_name}"
   end
 end
