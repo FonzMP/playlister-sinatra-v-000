02b04aa781e82e9eee79b261d29ecb69e50fabb1
Alex Wilson <fonzmp@gmail.com>
2018-06-19 21:54:59 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,7 +16,7 @@ class SongsController < Sinatra::Base
 
   post '/songs' do
 
-    artist = Artist.find_by(name: params[:artist][:name])
+    
 
     if !params[:artist][:name].empty?
       binding.pry
@@ -28,6 +28,8 @@ class SongsController < Sinatra::Base
 
       @artist.save
       @song.save
+    else
+      artist = Artist.find_by(name: params[:artist][:name])
     end
 
     flash[:message] = "Successfully created song."
