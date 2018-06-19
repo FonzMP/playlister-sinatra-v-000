c586c7a87d6aeec273b0571c45133706c9a18bb3
Alex Wilson <fonzmp@gmail.com>
2018-06-19 21:50:31 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -27,6 +27,8 @@ class SongsController < Sinatra::Base
       @song.save
     end
 
+    binding.pry
+
     flash[:message] = "Successfully created song."
     redirect "/songs/#{@song.slug}"
   end
